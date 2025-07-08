e3d9d45fddb104f8d4d0bf42c4e8d6c8c746f08c
besu178 <besuwinner@gmail.com>
2025-07-08 22:08:07 +0300

    finished seeding and scaffolding the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+10.times do |i|
+  Todo.create!(
+    task: "Todo #{i + 1} This is the description for Todo #{i + 1}",
+  )
+end
